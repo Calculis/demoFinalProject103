31c5c27541d97c22406b138fc7e90f51f40e74ff
Phunon.Sriv <phunon.sriv@mail.kmutt.ac.th>
2021-03-01 15:22:33 +0700

    Done// Format , number as String

--- demoProject103/src/demoproject103/DemoProject103.java
+++ demoProject103/src/demoproject103/DemoProject103.java
@@ -4,8 +4,10 @@
  * and open the template in the editor.
  */
 package demoproject103;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+
 /**
  *
  * @author phunon
@@ -17,15 +19,18 @@ public class DemoProject103 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-     VeryLongInteger b1=new VeryLongInteger("30000000");
-     VeryLongInteger b2=new VeryLongInteger("1200000000000000");
-    
+        VeryLongInteger b1 = new VeryLongInteger("30000000");
+        VeryLongInteger b2 = new VeryLongInteger("1200000000000000");
+        VeryLongInteger a1 = new VeryLongInteger("4000");
+        VeryLongInteger a2 = new VeryLongInteger("2000");
+
         System.out.println(b1);
         System.out.println(b2);
         System.out.println(b1.sum(b2));
-        System.out.println(b2.sum(b1));
-      
-       
+        System.out.println(b2.sum(b1).setFormat());
+        System.out.println(a1.sum(a2).setFormat());
+        System.out.println(a1.setFormat());
+
     }
-    
+
 }
--- demoProject103/src/demoproject103/VeryLongInteger.java
+++ demoProject103/src/demoproject103/VeryLongInteger.java
@@ -4,70 +4,66 @@
  * and open the template in the editor.
  */
 package demoproject103;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  *
  * @author phunon
  */
-public class VeryLongInteger  implements DataSet{
+public class VeryLongInteger implements DataSet {
 
-    ArrayList<Integer> veryLongInteger=new ArrayList<>();
+    ArrayList<Integer> veryLongInteger = new ArrayList<>();
 
     public VeryLongInteger(String number) {
         setArray(number);
     }
-    
-    
+
     @Override
     public VeryLongInteger sum(Object numSet) {
-        VeryLongInteger numSet2=(VeryLongInteger) numSet;
-        boolean exceed=false;
+        VeryLongInteger numSet2 = (VeryLongInteger) numSet;
+        boolean exceed = false;
         trim(numSet2);
-        ArrayList<Integer> result=zeroArray(this.veryLongInteger.size());
-        int buffer=0;
-        
-        for(int i=(this.veryLongInteger.size()-1);i>=0;i--){ // biggest loop
+        ArrayList<Integer> result = zeroArray(this.veryLongInteger.size());
+        int buffer = 0;
+
+        for (int i = (this.veryLongInteger.size() - 1); i >= 0; i--) { // biggest loop
             //condition
-             buffer=this.veryLongInteger.get(i)+numSet2.veryLongInteger.get(i);
-             if(i==0&& (buffer>=10||(buffer+1>=10&& exceed))){
-                 if(exceed){
-                     result.set(i,buffer-10+1);
-                     result.add(0,1);
-                 }
-                 else{
-                     result.set(i, buffer-10);
-                     result.add(0,1);
-                 }
-             }
-             else if(buffer+1>=10 && exceed){
-               result.set(i, buffer-10+1);
-               exceed=true;
-               
+            buffer = this.veryLongInteger.get(i) + numSet2.veryLongInteger.get(i);
+            if (i == 0 && (buffer >= 10 || (buffer + 1 >= 10 && exceed))) {
+                if (exceed) {
+                    result.set(i, buffer - 10 + 1);
+                    result.add(0, 1);
+                } else {
+                    result.set(i, buffer - 10);
+                    result.add(0, 1);
+                }
+            } else if (buffer + 1 >= 10 && exceed) {
+                result.set(i, buffer - 10 + 1);
+                exceed = true;
+
+            } else if (buffer + 1 < 10 && exceed) {
+                result.set(i, buffer + 1);
+                exceed = false;
+            } else if (buffer < 10 && !exceed) {
+                result.set(i, buffer);
+                exceed = false;
+            } else if (buffer >= 10) {
+                result.set(i, buffer - 10);
+                exceed = true;
             }
-           else if(buffer+1<10 && exceed){
-               result.set(i, buffer+1);
-               exceed=false;
-           }
-           else if(buffer<10 && !exceed){
-               result.set(i, buffer);
-               exceed=false;
-           }
-           else if(buffer>=10){
-               result.set(i,buffer-10);
-               exceed=true;
-           }
-            
-            
+
         }
-        
-      String temp="";
-      for(int a:result){
-          temp+= String.valueOf(a);
-      }
-      VeryLongInteger v=new VeryLongInteger(temp);
-      v.trimOut(v);
-       
-       return v;
+
+        String temp = "";
+        for (int a : result) {
+            temp += String.valueOf(a);
+        }
+        VeryLongInteger v = new VeryLongInteger(temp);
+        v.trimOut(v);
+
+        return v;
     }
 
     @Override
@@ -81,81 +77,115 @@ public class VeryLongInteger  implements DataSet{
     }
 
     @Override
-    public Object divided(Object numSet ,int by) {
+    public Object divided(Object numSet, int by) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Object power(Object numSet,int raise) {
+    public Object power(Object numSet, int raise) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String toString() {
 //        trimOut(this);
-        String temp="";
-        for(int a:veryLongInteger){
-            temp+=a;
+        String temp = "";
+        for (int a : veryLongInteger) {
+            temp += a;
         }
         return temp;
-    }  
+    }
 
     @Override
     public void setArray(String number) {
         int temp;
         this.veryLongInteger.clear();
-        for(int i=0;i<number.length();i++){
-            temp=Integer.parseInt(String.valueOf(number.charAt(i))); 
+        for (int i = 0; i < number.length(); i++) {
+            temp = Integer.parseInt(String.valueOf(number.charAt(i)));
             this.veryLongInteger.add(temp);
         }
-       
+
     }
-   public int grabIndex(int index){
-       return this.veryLongInteger.get(index);
-   }
-               
+
+    public int grabIndex(int index) {
+        return this.veryLongInteger.get(index);
+    }
+
     public ArrayList<Integer> getVeryLongInteger() {
         return veryLongInteger;
     }
-    public void trim(VeryLongInteger e){
-       if(e.getVeryLongInteger().size()==this.veryLongInteger.size()) return;
-            while(e.getVeryLongInteger().size()>this.veryLongInteger.size()){
-                this.veryLongInteger.add(0,0);
-            }
-            while(e.getVeryLongInteger().size()<this.veryLongInteger.size()){
-                e.getVeryLongInteger().add(0,0);
-            }
-        e.getVeryLongInteger().add(0,0);
-        this.veryLongInteger.add(0,0);
+
+    public void trim(VeryLongInteger e) {
+        if (e.getVeryLongInteger().size() == this.veryLongInteger.size()) {
+            return;
+        }
+        while (e.getVeryLongInteger().size() > this.veryLongInteger.size()) {
+            this.veryLongInteger.add(0, 0);
+        }
+        while (e.getVeryLongInteger().size() < this.veryLongInteger.size()) {
+            e.getVeryLongInteger().add(0, 0);
+        }
+        e.getVeryLongInteger().add(0, 0);
+        this.veryLongInteger.add(0, 0);
     }
-    public ArrayList<Integer> zeroArray(int size){
-        ArrayList<Integer> temp=new ArrayList<>();
-        for(int i=0;i<size;i++){
+
+    public ArrayList<Integer> zeroArray(int size) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
             temp.add(0);
         }
         return temp;
     }
-    public VeryLongInteger castFromObj (Object temp){
-       VeryLongInteger a=(VeryLongInteger) temp;
-         return a;
-       
+
+    public VeryLongInteger castFromObj(Object temp) {
+        VeryLongInteger a = (VeryLongInteger) temp;
+        return a;
+
     }
-    public void trimOut(VeryLongInteger e){
-        
-        int terminate=0;
-        
-        for(int i=0;i<e.veryLongInteger.size();i++){
-            if(e.veryLongInteger.get(i)!=0){
-                terminate=i;
+
+    public void trimOut(VeryLongInteger e) {
+
+        int terminate = 0;
+        if (e.veryLongInteger.get(0) != 0) {
+            return;
+        }
+        for (int i = 0; i < e.veryLongInteger.size(); i++) {
+
+            if (e.veryLongInteger.get(i) != 0) {
+                terminate = i;
                 break;
             }
-            
-        }
-        for(int k=0;k<terminate;k++){
-            e.veryLongInteger.remove(k);
-      }
-    }
-    
-}
 
-    
+        }
+
+        for (int k = 0; k < terminate; k++) {
+            e.veryLongInteger.remove(k);
+        }
+    }
+    public String setFormat(){
+        int temp= this.toString().length()-1;
+        ArrayList<Character> result= new ArrayList<>();
+        
+        for(int l=0;l<this.toString().length();l++){
+            result.add(toString().charAt(l));
+        }
+        
+        int counter=0;
+        for(int i=temp;i>=0;i--){
+            counter++;
+            
+            
+            if(counter%3==0){
+                counter=0;
+                result.add(i,',');
+                
+            }
+        }
+        String tempS="";
+        for(char a:result){
+            tempS+=a;
+        }
+        return tempS;
+    }
+
+}
